b59450d726fecf19b57c5aa98f1661cf94655a80
Fabricio <fabriciospfullstack@gmail.com>
2022-11-10 21:14:17 -0300

    :sparkles: feat: Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { HeaderContainer, HeaderContent } from "./styles";
+
+import igniteLogo from '../../assets/logo.svg'
+import { Button } from "../Button";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={igniteLogo} alt="Dois triângulos verdes com um sobreposto acima e com um pequeno triângulo verde claro no centro." />
+
+        <Button size="medium">Nova Transação</Button>    
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
